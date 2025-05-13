01f37838c4949e0d35f368be02ae02d7cbf84264
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-05-13 16:39:01 -0300

    feat: add login and registration templates with Tailwind CSS styling

--- users/forms.py
+++ users/forms.py
@@ -9,9 +9,21 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Adiciona classes do Tailwind aos campos
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500'
+
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Adiciona classes do Tailwind aos campos
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500'
